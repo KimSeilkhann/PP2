f3590ed8a3b6a8da7f0718edf8b338cc4d54aed2
KimSeilkhann <kimseilkhan@mail.ru>
2019-01-28 16:19:42 +0600

    yo

---
+++ week 1/task2/task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace task2
+{
+    class Student       //создаем класс студент
+    {
+       
+            public string name;             // объявляем переменные 
+            public string id;
+            public int year;
+
+            public Student(string name, string id)      // создаем конструктор который принимает 2 переменные имя и айди
+        {
+            this.name = name;
+            this.id = id;
+        }
+        public Student(int year)        //создаем конструктор который принимает переменную год
+        {
+            this.year = year;
+        }
+        public void Yearr()         // функция для инкремента года
+        {
+           year++;
+        }
+        public void Cout()      //функция для вывода перемиенных 
+        {
+            Console.WriteLine(name + " " + id + " " + year);
+        }
+    }
+    class Program { 
+    public static void Main(string[] args)
+        {
+            string a = Console.ReadLine();      //считываем стринг, разделяем на 3 стринга
+            string[] s = a.Split();
+            Student kakawka = new Student(s[0],s[1]); // создаем переменную студент для имени и айди
+            kakawka.year = int.Parse(s[2]);     // парсим в инт третий стринг и создаем переменную студент для года 
+            kakawka.Yearr();        // вызываем функцию вывода всех переменных
+        }
+    }
+}
