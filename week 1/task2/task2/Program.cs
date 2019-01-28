d5e71f49fc7f35107357d06327231d1b8e5a07a6
KimSeilkhann <kimseilkhan@mail.ru>
2019-01-28 11:29:27 +0600

    kek

---
+++ week 1/task2/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i=0; i<n; i++)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    if (i >= j) Console.Write("[*]");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
