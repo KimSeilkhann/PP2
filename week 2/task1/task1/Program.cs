5410ccebe965c1df6aa4ae94bed6cd142874913e
KimSeilkhann <kimseilkhan@mail.ru>
2019-02-02 10:17:15 +0600

    fif

---
+++ week 2/task1/task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+namespace task1
+{
+    class MainClass
+    {
+        public static void kek()
+        {
+            StreamReader sr = new StreamReader("../../input.txt");
+            string s = sr.ReadToEnd();
+            string[] a = s.Split();
+            int l = a.Length;
+            for(int i=0; i<l/2; i++)
+            {
+                if (a[i] != a[l - i - 1])
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+
+            }
+
+            Console.WriteLine("Yes");
+
+        }
+        public static void Main(string[] args)
+        {
+            kek();
+        }
+    }
+}
