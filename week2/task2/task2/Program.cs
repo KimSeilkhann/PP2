0f59f23f6b3a27616a43370b0b589043254cfd00
KimSeilkhann <kimseilkhan@mail.ru>
2019-02-07 14:17:41 +0600

    new

---
+++ week2/task2/task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+namespace task2
+{
+    class MainClass
+    {
+        public static bool Pr(int n)
+        {
+            for (int i = 2; i <= Math.Sqrt(n); i++)     //функция для проверки на прайм
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("../../input.txt");      //считываем стринг через файл 
+            String b = sr.ReadToEnd();
+            String[] s = b.Split();         //делим стринг в массив 
+            sr.Close();
+            string ans = "";            
+            foreach (String a in s)
+            {
+                int c = int.Parse(a);       //проегаемся по массиву, каждый элемент парсим в инт и проверяем на прайм
+                if (Pr(c) == true) ans += a + " ";  //если число прайм добавляем в стринг ответ
+            }
+            StreamWriter sw = new StreamWriter("../../output.txt");     //через файл айтпут выводим ответ
+            sw.WriteLine(ans);
+            sw.Close();
+
+        }
+    }
+}
