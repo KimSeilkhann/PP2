64ec2f79180fccb68b16bb0c2f803054d7f834ec
KimSeilkhann <kimseilkhan@mail.ru>
2019-03-06 16:10:26 +0600

    syraya versiya zmeiki

---
+++ week6/SnakeGame/SnakeGame/Game.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SnakeGame
+{
+    public class Game
+    {
+        List<GameObject> g_objects;
+        public bool isAlive;
+        public Snake snake;
+        public Food food;
+        public Wall wall;
+
+        public Game()
+        {
+            g_objects = new List<GameObject>();
+            snake = new Snake(20, 10, '*', ConsoleColor.DarkBlue);
+            food = new Food(0, 0, 'o', ConsoleColor.Black);
+            wall = new Wall('#', ConsoleColor.Green);
+            wall.LoadLevel();
+            while (food.IsCollisionWithObject(snake) || food.IsCollisionWithObject(wall))
+                food.Generate();
+
+            g_objects.Add(snake);
+            g_objects.Add(food);
+            g_objects.Add(wall);
+
+            isAlive = true;
+        }
+
+        public void Start()
+        {
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            Console.WriteLine("Hello! Please, write your username");
+            Console.WriteLine(Console.WindowWidth + " " + Console.WindowHeight);
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.Clear();
+            while (isAlive && keyInfo.Key != ConsoleKey.Escape)
+            {
+                Draw();
+                keyInfo = Console.ReadKey();
+                if (snake.IsCollisionWithObject(food))
+                {
+                    snake.body.Add(new Point(0, 0));
+                    while (food.IsCollisionWithObject(snake) || food.IsCollisionWithObject(wall))
+                        food.Generate();
+
+                    if (snake.body.Count % 3 == 0)
+                        wall.NextLevel();
+                }
+                if (snake.IsCollisionWithObject(wall))
+                {
+                    isAlive = false;
+                }
+                snake.Move(keyInfo);
+            }
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(20, 10);
+            Console.WriteLine("GAME OVER!!!");
+        }
+        public void Draw()
+        {
+            Console.Clear();
+            foreach (GameObject g in g_objects)
+                g.Draw();
+        }
+    }
+}
---
+++ week6/SnakeGame/SnakeGame/Snake.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace SnakeGame
+{
+    public class Snake : GameObject
+    {
+        enum Direction
+        {
+            NONE,
+            RIGHT,
+            LEFT,
+            UP,
+            DOWN
+        }
+        Direction direction = Direction.NONE;
+        public bool ok;
+        
+        public Snake(int x, int y, char sign, ConsoleColor color) : base(x, y, sign, color)
+        {
+
+        }
+        public void CheckDirection(ConsoleKeyInfo keyInfo)
+        {
+            if (direction == Direction.NONE)
+            {
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                    direction = Direction.UP;
+                if (keyInfo.Key == ConsoleKey.DownArrow)
+                    direction = Direction.DOWN;
+                if (keyInfo.Key == ConsoleKey.LeftArrow)
+                    direction = Direction.LEFT;
+                if (keyInfo.Key == ConsoleKey.RightArrow)
+                    direction = Direction.RIGHT;
+            }
+          
+        }
+        public void CanUMove(ConsoleKeyInfo keyInfo)
+        { 
+            if (keyInfo.Key == ConsoleKey.UpArrow && direction == Direction.DOWN)
+            {
+                ok = false;
+                return;
+            }
+            if (keyInfo.Key == ConsoleKey.DownArrow && direction == Direction.UP)
+            {
+                ok = false;
+                return;
+            }
+            if (keyInfo.Key == ConsoleKey.LeftArrow && direction == Direction.RIGHT)
+            {
+                ok = false;
+                return;
+            }
+            if (keyInfo.Key == ConsoleKey.RightArrow && direction == Direction.LEFT)
+            {
+                ok = false;
+                return;
+            }
+            ok = true;
+        }
+        public void Move(ConsoleKeyInfo keyInfo)
+        {
+            CheckDirection(keyInfo);
+            CanUMove(keyInfo);
+         
+            if (ok)
+            {
+                for (int i = body.Count - 1; i > 0; i--)
+                {
+                    body[i].x = body[i - 1].x;
+                    body[i].y = body[i - 1].y;
+                }
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                    body[0].y--;
+                if (keyInfo.Key == ConsoleKey.DownArrow)
+                    body[0].y++;
+                if (keyInfo.Key == ConsoleKey.LeftArrow)
+                    body[0].x--;
+                if (keyInfo.Key == ConsoleKey.RightArrow)
+                    body[0].x++;
+                direction = Direction.NONE;
+                CheckDirection(keyInfo);
+            }
+          
+        }
+      
+    }
+}
